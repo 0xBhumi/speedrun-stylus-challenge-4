fe30329e6372e4795e97ae15def3ba55b58ffa67
0xbhumi <bhumisadariya@lampros.tech>
2025-09-03 17:02:10 +0000

    update code for challenge 4

--- packages/cargo-stylus/multi-sig/src/lib.rs
+++ packages/cargo-stylus/multi-sig/src/lib.rs
@@ -1,6 +1,7 @@
 // Allow `cargo stylus export-abi` to generate a main function.
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
+use stylus_cache_sdk::{is_contract_cacheable};
 
 /// Import items from the SDK. The prelude contains common traits and macros.
 use stylus_sdk::{contract, evm, msg, prelude::*, call::{Call, call}, alloy_primitives::{Address, U256}, abi::Bytes};
@@ -331,4 +332,8 @@ impl MultiSig {
         }
         Ok(owners_vec)
     }
+    pub fn is_cacheable(&self) -> bool {
+        is_contract_cacheable()
+    }
+
 }
